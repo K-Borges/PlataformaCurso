f30b30ec1e725eec80298d3601f4c5a190f103bd
carlosvtsdev@gmail.com <carlosvtsdev@gmail.com>
2026-02-27 13:45:56 -0300

    feat: Formulário para envio de pdfs

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class ListaExercicios(models.Model):
+    titulo = models.CharField(max_length=255)
+    arquivo = models.FileField(upload_to="listas/")
+    materia = models.CharField(max_length=100)
+    professor = models.CharField(max_length=100)
+    data_upload = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
